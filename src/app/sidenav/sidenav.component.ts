365eb007e848b14c22842a560a7ca7059add35c0
brampeirs <bram.peirs@showpad.com>
2024-12-12 20:46:05 +0100

    setup basic routing

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidenav',
+  imports: [RouterModule],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss',
+})
+export class SidenavComponent {}
